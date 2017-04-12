3d2ab3de865a943ea620dc41dc7c1746a7aa8d7f
Kamil <kaqil91@gmail.com>
2017-04-11 18:57:16 -0700

    got dat file structure

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// The game starts by prompting the user to select a character
+// from a seclection of four characters.
+
+// The screen will then promp the user to select an enemy to
+// attack from the remaining 3 characters
+
+// Once the enemy is selected the page will change to the fight
+// screen with only the users character and enemy choice displayed
+// there will be two buttons, one to fight and one to run if the
+// chosen enemy is too strong. that will  link back to the enemy
+// select screen 
