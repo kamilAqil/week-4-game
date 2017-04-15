1cf03263fe52f564e7088dc2f9cf0ecf2877e752
Kamil <kaqil91@gmail.com>
2017-04-14 19:43:49 -0700

    user selection works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,74 @@
 
-var userSelection="";
-var enemySelection="";
-var characters = ['squirtle','charmander','bulbasaur','pikachu'];
+// the game toggles between two screens the
+// select screen and the fight screen
 
-  // when a character is clicked, if userSelection is not set
-  // we will assign the value based on which character was clicked
-  // if userSelection is assigned we will assign enemySelection
-  $('.character').on('click',function(){
-    if(userSelection !== "" && enemySelection !== ""){
-      console.log("all set");
+
+var game = {
+  "characters": {
+    "squirtle": {
+      "name": "squirtle",
+      "imageSource":"./assets/images/Squirtle.png",
+      "health": 100,
+      "attack": 10,
+      "counterAttack": 5
+    },
+    "charmander":{
+      "name": "charmander",
+      "imageSource":"./assets/images/Charmander.png",
+      "health": 100,
+      "attack": 10,
+      "counterAttack": 5
+    },
+    "bulbasaur":{
+      "name": "bulbasaur",
+      "imageSource":"./assets/images/bulbasaur.png",
+      "health": 100,
+      "attack": 10,
+      "counterAttack": 5
+    },
+    "pikachu":{
+      "name": "pikachu",
+      "imageSource":"./assets/images/pikachu.png",
+      "health": 100,
+      "attack": 10,
+      "counterAttack": 5
+    },
+  },
+  "userSelection":undefined,
+  // this is just an array of strings containing the names of enemies
+  "enemies":[],
+  "userEnemySelection":undefined,
+};
+
+$('.character').on('click',function(){
+
+  var selectionName = this.id;
+
+  // if the userSelection is undefined after the
+  // character click event update the userSelection with
+  // the character object
+  if(game.userSelection==undefined){
+    game.userSelection = game.characters[selectionName];
+    console.log("userSelection is "+game.userSelection.name);
+    $('#mainContentSelectHeader').html("Select Enemy");
+
+    // for each character in game.characters if the character.name
+    // is not == to game.userSelection.name push character to game.enemies
+    // array
+    for(character in game.characters){
+      if(character !== game.userSelection.name){
+        game.enemies.push(character);
+      }
     }
-    else if(userSelection==""){
-      var name = $(this).attr("id");
-      // console.log(name);
-      userSelection = name;
-      console.log("your selection: "+userSelection);
-    }
-    else if(userSelection !== ""){
-      var name = $(this).attr("id");
-      // console.log(name);
-      enemySelection = name;
-      console.log("enemy selection: "+enemySelection);
-    }
-  });
+    console.log(game.enemies);
+  }
+
+});
+
+
+// pass this function the name of the character(string)
+// to update the health bar for the corresponding character
+function setHealth(character,value){
+  $('#'+character+'HealthBar').html(value+"%");
+  $('#'+character+'HealthBar').css("width",value);
+}
